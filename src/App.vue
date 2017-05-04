28abdc63a105b44025915cdb92589186cc4d06d2
Jeff Mosawy <jmosawy@gmail.com>
2017-05-04 15:11:25 +0430

    Main project structure is being built.
    - Updated package.json scripts.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper" id="app">
+    HELLO WORLD
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-new */
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+import Style from './style.scss';
+
+const app = new Vue({
+  el: '#app',
+  render: h => h(App),
+});
